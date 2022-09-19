fe85bf2471a6ff9598288602828d3e49c64d7cc3
Todor Arnaudovski <todorarnaudovski@gmail.com>
2022-09-19 14:20:14 +0200

    Add content controls and icons

--- src/features/AvailableQuests/components/QuestItem/QuestItem.js
+++ src/features/AvailableQuests/components/QuestItem/QuestItem.js
@@ -3,6 +3,8 @@ import { CurrentQuestsContext } from 'context/CurrentQuestsContext';
 
 import styles from './QuestInfo.module.scss';
 
+import { MdExpandMore, MdExpandLess, MdAdd } from 'react-icons/md';
+
 import { List } from 'components/List';
 import { ListItem } from 'components/ListItem';
 import { Badge } from 'components/Badge/Badge';
@@ -12,11 +14,16 @@ import { Button } from 'components/Button';
 export const QuestItem = (props) => {
   const { id, title, description, level, isShareable, prerequisites } = props;
   const isShareableText = isShareable ? 'Shareable' : 'Not shareable';
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const [isAdded, setIsAdded] = useState(false);
 
   const { addQuest } = useContext(CurrentQuestsContext);
 
-  const addItemToCurrentQuestsHandler = () => {
+  const collapseItemHandler = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  const addItemHandler = () => {
     setIsAdded(true);
 
     const timer = setTimeout(() => {
@@ -41,24 +48,29 @@ export const QuestItem = (props) => {
       <Badge position='top-left' color={`${isShareable ? 'success' : 'danger'}`}>
         {isShareableText}
       </Badge>
-      <Button
-        className={styles['add-button']}
-        variant='success'
-        onClick={addItemToCurrentQuestsHandler}
-      >
-        +
-      </Button>
-      <h4 className='h4'>{title}</h4>
-      <p className='mb-1'>{description}</p>
-      <LevelInfo level={level} />
-      {prerequisites.length > 0 && (
-        <div>
-          <h5 className='h5'>Prerequisites:</h5>
-          <List variant='unstyled'>
-            {prerequisites.map((prerequisite, i) => {
-              return <ListItem key={`${id}-${i}`}>{prerequisite}</ListItem>;
-            })}
-          </List>
+      <div className={styles['content-controls']}>
+        <Button variant='warning' onClick={collapseItemHandler}>
+          {isCollapsed ? <MdExpandMore /> : <MdExpandLess />}
+        </Button>
+        <Button variant='success' onClick={addItemHandler} disabled={isAdded}>
+          <MdAdd />
+        </Button>
+      </div>
+      <h4 className={`${isCollapsed ? 'h4' : 'h3'}`}>{title}</h4>
+      {!isCollapsed && (
+        <div className={styles['quest-body']}>
+          <p className='mb-1'>{description}</p>
+          <LevelInfo level={level} />
+          {prerequisites.length > 0 && (
+            <div>
+              <h5 className='h5'>Prerequisites:</h5>
+              <List variant='unstyled'>
+                {prerequisites.map((prerequisite, i) => {
+                  return <ListItem key={`${id}-${i}`}>{prerequisite}</ListItem>;
+                })}
+              </List>
+            </div>
+          )}
         </div>
       )}
     </div>
--- src/features/CurrentQuests/components/QuestItem/QuestItem.js
+++ src/features/CurrentQuests/components/QuestItem/QuestItem.js
@@ -3,6 +3,8 @@ import { CurrentQuestsContext } from 'context/CurrentQuestsContext';
 
 import styles from './QuestInfo.module.scss';
 
+import { MdExpandMore, MdExpandLess, MdRemove } from 'react-icons/md';
+
 import { List } from 'components/List';
 import { ListItem } from 'components/ListItem';
 import { Badge } from 'components/Badge/Badge';
@@ -12,9 +14,14 @@ import { Button } from 'components/Button';
 export const QuestItem = (props) => {
   const { id, title, description, level, isShareable, prerequisites } = props;
   const isShareableText = isShareable ? 'Shareable' : 'Not shareable';
+  const [isCollapsed, setIsCollapsed] = useState(true);
   const [isRemoved, setIsRemoved] = useState(false);
 
   const { removeQuest } = useContext(CurrentQuestsContext);
+
+  const collapseItemHandler = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
   const removeItemHandler = () => {
     setIsRemoved(true);
@@ -41,20 +48,29 @@ export const QuestItem = (props) => {
       <Badge position='top-left' color={`${isShareable ? 'success' : 'danger'}`}>
         {isShareableText}
       </Badge>
-      <Button className={`${styles['remove-button']}`} variant='danger' onClick={removeItemHandler}>
-        x
-      </Button>
-      <h4 className='h4'>{title}</h4>
-      <p className='mb-1'>{description}</p>
-      <LevelInfo level={level} />
-      {prerequisites.length > 0 && (
-        <div>
-          <h5 className='h5'>Prerequisites:</h5>
-          <List variant='unstyled'>
-            {prerequisites.map((prerequisite, i) => {
-              return <ListItem key={`${id}-${i}`}>{prerequisite}</ListItem>;
-            })}
-          </List>
+      <div className={styles['content-controls']}>
+        <Button variant='warning' onClick={collapseItemHandler}>
+          {isCollapsed ? <MdExpandMore /> : <MdExpandLess />}
+        </Button>
+        <Button variant='danger' onClick={removeItemHandler} disabled={isRemoved}>
+          <MdRemove />
+        </Button>
+      </div>
+      <h4 className={`${isCollapsed ? 'h4' : 'h3'}`}>{title}</h4>
+      {!isCollapsed && (
+        <div className={styles['quest-body']}>
+          <p className='mb-1'>{description}</p>
+          <LevelInfo level={level} />
+          {prerequisites.length > 0 && (
+            <div>
+              <h5 className='h5'>Prerequisites:</h5>
+              <List variant='unstyled'>
+                {prerequisites.map((prerequisite, i) => {
+                  return <ListItem key={`${id}-${i}`}>{prerequisite}</ListItem>;
+                })}
+              </List>
+            </div>
+          )}
         </div>
       )}
     </div>
